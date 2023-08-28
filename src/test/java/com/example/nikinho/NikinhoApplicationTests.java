195179071c02a41d9f008e6f70f234b6075f46eb
Nicolas Granatelli Sanches <99562632+Nicolas-Granatelli@users.noreply.github.com>
2023-08-28 18:57:20 -0300

    Add files via upload

---
+++ src/test/java/com/example/nikinho/NikinhoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nikinho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NikinhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
