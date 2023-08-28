f5b7c75099191a0c1b86989baf8901a61bb84760
Nicolas Granatelli Sanches <99562632+Nicolas-Granatelli@users.noreply.github.com>
2023-08-28 22:48:36 +0000

    Nikinho

--- src/main/java/com/example/nikinho/NikinhoApplication.java
+++ src/main/java/com/example/nikinho/NikinhoApplication.java
@@ -4,10 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RestController
 public class NikinhoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NikinhoApplication.class, args);
+	}
+	@GetMapping("/nikinho")
+	public String hello(){
+		return "Seja bem vindo Nikinho!";
 	}
 
 }
